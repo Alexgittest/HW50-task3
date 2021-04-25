3915a4ba58489b855849da71a63ae99d15bc860a
Aliaksandr <39026425+Alexgittest@users.noreply.github.com>
2021-04-25 17:52:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,48 +1,50 @@
-#!/app/venv/bin/python
-
 from flask import Flask,request
 import json
 from datetime import date
+import boto3
+
 
 today = date.today()
 app=Flask(__name__)
 
-@app.route("/",methods=['GET', 'POST'])
+@app.route('/',methods=['GET', 'POST'])
 def index():
-    with open("./birthdays.txt",'r') as filename:
-        birthday_list=filename.read()
-        data=json.loads(birthday_list)
+    s3 = boto3.resource('s3',verify=False)
+    obj = s3.Object('flask_app_bucket', 'birthday.txt')
+    data=obj.get()['Body'].read().decode('utf-8')
+    people_dict=json.loads(data)
     if request.method == 'POST':
         user=request.form['username']
         dr=request.form['dr']
-        with open("./birthdays.txt",'w') as filename:
-            data[user]=dr
-            filename.write(json.dumps(data))
-        return "Add new user \n"
+        people_dict[user]=dr
+        people_str=json.dumps(people_dict)
+        str(people_dict).encode()
+        obj.put(Body=people_str.encode())
+        return "Add new user"
     else:       
-        return "Hello it is flask app \n"
+        return "Hello it is flask app"
 
-@app.route("/<username>")
+@app.route('/<username>')
 def show_user_birthday(username):
-    with open("./birthdays.txt",'r') as filename:
-        birthday_list=filename.read()
-        data=json.loads(birthday_list)
-        birthday="Совпадений не найдено \n"   
-        for i in data.keys():
-            if i == username:
-                birthday=data[i].split(".")
-                dr=date(today.year,int(birthday[1]),int(birthday[0]))
-                delta=(today-dr).days
-                if 0<delta<3:
-                    birthday="ДР было меньше чем 3 дня назад \n"
-                elif -3<delta<0:
-                    birthday="ДР будет через меньше чем через 3 дня \n"
-                elif delta==0:
-                    birthday="ДР Сегодня! \n"
-                else:
-                    birthday="ДР еще не скоро \n"
+    s3 = boto3.resource('s3',verify=False)
+    obj = s3.Object('flask_app_bucket', 'birthday.txt')
+    data=obj.get()['Body'].read().decode('utf-8')
+    people_dict=json.loads(data)
+    birthday="Совпадений не найдено"   
+    for i in people_dict.keys():
+        if i == username:
+            birthday=people_dict[i].split(".")
+            dr=date(today.year,int(birthday[1]),int(birthday[0]))
+            delta=(today-dr).days
+            if 0<delta<3:
+                birthday="ДР было меньше чем 3 дня назад"
+            elif -3<delta<0:
+                birthday="ДР будет через меньше чем через 3 дня"
+            elif delta==0:
+                birthday="ДР Сегодня!"
+            else:
+                birthday="ДР еще не скоро"
     return birthday
-
 
 if __name__=="__main__":
     app.run(port=5000, host='0.0.0.0')
